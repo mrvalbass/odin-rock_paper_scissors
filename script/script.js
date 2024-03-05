01d7cbf79213328e73ba2cbea605f6454fd0974e
Valentin Guillot <97942568+mrvalbass@users.noreply.github.com>
2024-03-05 14:34:24 +0100

    Refactor playGame()
    Added or modified helper functions
    capitalize() : self explanatory,
    getPlayerChoice() : moved the validation of user input here to make playGame() more readable
    
    Minor code readability improvement

--- script/script.js
+++ script/script.js
@@ -1,7 +1,17 @@
 // Create a game of Rock, Paper, Scissors
 
+// Capitalize a word
+function capitalize(word) {
+    return word.at(0).toUpperCase() + word.slice(1).toLowerCase()
+}
+
+// Format the game data for better readability
+function formatGameData(gamesWon, gamesLost) {
+    return `Games won : ${gamesWon} | Games Lost : ${gamesLost}`
+}
+
 // Get the computer to choose randomly between rock, paper and scissors
-function getComputerChoice () {
+function getComputerChoice() {
     switch (Math.floor(Math.random()*3)){
         case 0:
             return 'rock'
@@ -13,17 +23,21 @@ function getComputerChoice () {
 }
 
 // Get the user input
-function getPlayerChoice (gamesLeft, gameData) {
-    if (gamesLeft > 1) {
-        return prompt(`Welcome to Odin Rock, Paper, Scissors game.\nIt is a BO5 game.\nThere are ${gamesLeft} games left to play.` + gameData + `\nChoose your weapon :`).toLowerCase()
-    } else {
-        return prompt(`Welcome to Odin Rock, Paper, Scissors game.\nIt is a BO5 game against the computer.\nThere is ${gamesLeft} game left to play.` + gameData + `\nChoose your weapon :`).toLowerCase()
-    }
-}
-
-// Format the game data
-function getGameData (gamesWon, gamesLost) {
-    return `\nGames won : ${gamesWon} | Games Lost : ${gamesLost}`
+function getPlayerChoice(gamesLeft, gameData) {
+    let playerSelection;
+    do {
+        playerSelection = prompt(
+`Welcome to Odin Rock, Paper, Scissors game.
+It is a BO5 game.
+There ${gamesLeft > 1 ? `are ${gamesLeft} games` : `is ${gamesLeft} game`} left to play. 
+${gameData}
+Choose your weapon :`
+        ).toLowerCase().trim();
+        if (playerSelection !== 'rock' && playerSelection !== 'paper' && playerSelection !== 'scissors') {
+            alert('Incorrect input! Choose from Rock, Paper of Scissors');
+        };
+    } while (playerSelection !== 'rock' && playerSelection !== 'paper' && playerSelection !== 'scissors');
+    return playerSelection;
 }
 
 // Define winning, tie and losing cases according to player input and computer random choice
@@ -32,54 +46,42 @@ function playRound(playerSelection, computerSelection) {
         (playerSelection === 'rock' && computerSelection === 'scissors') ||
         (playerSelection === 'paper' && computerSelection === 'rock') ||
         (playerSelection === 'scissors' && computerSelection === 'paper')) {
-        return `You Won! ${playerSelection.at(0).toUpperCase() + playerSelection.slice(1)} beats ${computerSelection}` 
+        return `You Won! ${capitalize(playerSelection)} beats ${computerSelection}` 
     } else if (playerSelection === computerSelection) {
         return "It's a tie. Rematch!"
     } else {
-        return `You Lost! ${computerSelection.at(0).toUpperCase() + computerSelection.slice(1)} beats ${playerSelection}`
+        return `You Lost! ${capitalize(computerSelection)} beats ${playerSelection}`
     }
 }
 
-// Ask user for it's input that can only be rock, paper or scissors and play 5 games in a row
+// Play a BO5 Rock, Paper, Scissors game
 function playGame() {
-    //Initialize game data
+    // Game data
     let gamesWon = 0;
     let gamesLost = 0;
 
     for (let i = 0; i < 5; i++) {
-        // Initialize round data
+        // Round data
         const gamesLeft = 5 - i
-        let gameResult = 'tie'
+        let roundResult
 
-        //Rematch if the game is a tie
-        while (gameResult.includes('tie')) {
-            // Get user input
-            let playerSelection = getPlayerChoice(gamesLeft, getGameData(gamesWon, gamesLost))
+        //Rematch if the round is a tie
+        do {
+            let playerSelection = getPlayerChoice(gamesLeft, formatGameData(gamesWon, gamesLost))
+            roundResult = playRound(playerSelection,getComputerChoice());
 
-            // Check user input
-            while (playerSelection !== 'rock' && playerSelection !== 'paper' && playerSelection !== 'scissors') {
-                alert('Incorrect input! Choose from Rock, Paper of Scissors');
-                playerSelection = getPlayerChoice(gamesLeft, getGameData(gamesWon, gamesLost));
-            };
-
-            //Play the round
-            gameResult = playRound(playerSelection,getComputerChoice());
-
-            if (gameResult.includes('Won')) {
+            if (roundResult.includes('Won')) {
                 gamesWon++;
-            } else if (gameResult.includes('Lost')){
+            } else if (roundResult.includes('Lost')){
                 gamesLost++;
             };
 
-            // Output data to the user
-            alert(gameResult + getGameData(gamesWon, gamesLost));
-        }
+            alert(`${roundResult}\n${formatGameData(gamesWon, gamesLost)}`);
+        } while (roundResult.includes('tie'));
 
-        // Stop the game if 3 games have been won from the user or the computer
-        if (gamesWon === 3 || gamesLost === 3) {break}
+        if (gamesWon === 3 || gamesLost === 3) {break};
     };
 
-    // Output final message
     if(gamesWon > gamesLost) {
         alert('Congratulations, you beat the computer')
     } else {
